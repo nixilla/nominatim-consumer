3aa011e984e4e96d359082e0c069c21166b6fb84
Rodrigo Primo <rodrigo@hacklab.com.br>
2014-12-30 12:37:55 -0200

    add Query::setParam() to the example

--- example.php
+++ example.php
@@ -6,6 +6,7 @@ $consumer = new \Nominatim\Consumer($client, 'http://nominatim.openstreetmap.org
 
 $query = new \Nominatim\Query();
 $query->setQuery('pilkington avenue,birmingham');
+$query->setParam('addressdetails', 1);
 $result = $consumer->search($query);
 
 echo "\n===================\n";
@@ -32,3 +33,4 @@ foreach($result as $item)
     printf('%s @ lat:%s, lon: %s', $item['display_name'], $item['lat'], $item['lon']);
     echo "\n===================\n";
 }
+
